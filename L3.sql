217a6c5e964c39c71a0d440b5c762c198bbe7bda
mint trees <104205313+minttreesdao@users.noreply.github.com>
2024-07-12 23:14:23 +0200

    Update L3.sql
    base streak query

--- L3.sql
+++ L3.sql
@@ -44,5 +44,46 @@ and p.profile_id in ('0x017566', '0x0f85')
 and pu.profile_id is null
 
 order by p.profile_id, d.date desc
+;
+
+-- base streak query
+select 
+  base_rank.*
+from
+  (
+  select 
+    base.*
+  , rank() over (partition by profile_id order by streak desc, date desc) as streak_rank
+  from
+    (
+    select 
+      d.date
+    , p.profile_id
+    , lead(d.date) over (partition by p.profile_id order by d.date desc) as date_before
+    , date_diff(d.date, lead(d.date) over (partition by p.profile_id order by d.date desc), day) as streak
+    from `intellilens.L2_date` d
+    left join `intellilens.L2_profile` p
+    on 1 = 1
+    left join `intellilens.L2_publication` pu
+    on pu.publication_date = d.date
+    and pu.profile_id = p.profile_id
+    and pu.publication_type = 'POST'
+    and pu.publication_date < '2024-07-12'
+
+    and pu.profile_id  in ('0x0f85')
+    and pu.publication_date_year_month >= '2024-07-01'
+
+    where d.date <= '2024-07-12' -- day after last publication date (-> end date)
+    and d.date >= '2024-06-30'
+    and d.date >= '2022-05-16' -- day before first publication date (-> start date)
+
+    and p.profile_id in ('0x0f85')
+
+    and pu.profile_id is null
+    -- order by p.profile_id, d.date desc
+    ) base
+  ) base_rank
+where streak_rank = 1
+or date = '2024-07-12'
 
 
